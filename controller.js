ce4196688d5281eaf0fad80472a11f2bd452d690
Abbie Gonzalez <abbiegonzalez@Enterprise.local>
2025-03-14 22:33:34 -0400

    initial

---
+++ controller.js
@@ -0,0 +1,127 @@
+class controller {
+  #chartArea;
+  #settingsDialog;
+  warriorServerConnections;
+  warriorServerDisconnects;
+  #topLevel;
+  allSettings;
+
+
+  constructor() {
+    this.#topLevel = this;
+    this.allSettings = { nick : "", project: "", reuse: true};
+    this.#chartArea = document.getElementById('chartArea');
+    this.#settingsDialog = document.getElementById('settings');
+    this.warriorServerDisconnects = {};
+    this.warriorServerConnections = new Array();
+    
+    this.showSettings();
+    document.getElementById('settingsSave').addEventListener("click", this.saveSettings.bind(this));
+    this.disconnectsInterval(this.warriorServerDisconnects);
+  }
+
+  addDisconnect(disconnectList, serverName, serverURL) {
+    console.log(`${serverName} unavailable will check later.`);
+    disconnectList[serverName] = serverURL;
+  }
+
+  showSettings(){
+    this.#settingsDialog.classList.add("show");
+  }
+
+  hideSettings() {
+    this.#settingsDialog.classList.remove("show");
+  }
+
+  saveSettings() {
+    console.log(this.allSettings);
+
+    this.allSettings.nick = document.getElementById("inputNickName").value;
+    this.allSettings.project = document.getElementById("inputProject").value;
+    
+    var warriorServerString = document.getElementById("inputServerList").value;
+    var warriorServers = warriorServerString.split("\n");
+    var warriorServer = new Array;
+
+    warriorServers.forEach(serverItem => {
+      warriorServer = serverItem.split(",");
+      try{
+        this.warriorServerConnections.push(new anwarConnection( warriorServer[0].trim(), warriorServer[1], this.#chartArea, this));
+      } catch(ex) {
+        console.log(ex);
+      }
+    });
+    this.hideSettings();
+    this.updateMetrics(this.allSettings.nick, this.allSettings.project);
+  }
+
+  updateMetrics(name, project) {
+    fetch(`https://legacy-api.arpa.li/${project}/stats.json`, {
+      "method": "GET",
+      "headers": {}
+    })
+      .then((res) => res.text())
+      .then((stats) => {
+
+        var downloaderArray = new Array();
+        var currentDate = new Date().toLocaleString();
+        var fullStatData = JSON.parse(stats);
+        if (fullStatData.downloaders.indexOf(name) > 0) {
+          var statPlace = document.getElementById("metric");
+
+          for (const [key, value] of Object.entries(fullStatData.downloader_bytes)) {
+            downloaderArray.push({ "name": key, "dl": value });
+          }
+          downloaderArray.sort(function (a, b) {
+            return b.dl - a.dl;
+          });
+          var statPos = downloaderArray.findIndex(e => { return (e.name === name) });
+          var gigsDL = this.humanBytes( fullStatData.downloader_bytes[name] );
+          var statTotP = fullStatData.downloaders.length;
+          statPlace.innerHTML = `<h1>Downloading: ${project}</h1>
+                                <p>Data Saved: <span class=data>${gigsDL.toLocaleString()}</span></p>
+                                <p>Items Saved: <span class=data>${fullStatData.downloader_count[name].toLocaleString()}</span></p>
+                                <p>Position: <span class=data>${statPos} / ${statTotP}</span></p>
+                                <p>As of: <span class=data>${currentDate}</span></p>`;
+          this.updateTimer(name, project);
+        }
+
+
+      })
+      .then(console.log.bind(console))
+      .catch(console.error.bind(console));
+  }
+
+   humanBytes(bytes) {
+    if (bytes > 1024 * 1024 * 1024) {
+      return (Math.round(10 * bytes / (1024 * 1024 * 1024)) / 10) + ' GB';
+    } else if (bytes > 1024 * 1024) {
+      return (Math.round(10 * bytes / (1024 * 1024)) / 10) + ' MB';
+    } else {
+      return (Math.round(10 * bytes / (1024)) / 10) + ' kB';
+    }
+  }
+
+  disconnectsInterval(list) {
+    setInterval(()=> {
+      if(Object.keys(list).length > 0) {
+        for(const item in list) {
+          if(this.allSettings.sort){
+            document.getElementById(item).remove();
+          }
+          this.warriorServerConnections.push(new anwarConnection( item, list[item], this.#chartArea, this, this.allSettings.reuse));
+
+          delete list[item];
+        }
+      }
+    }, 60000);
+
+  }
+
+  updateTimer(nick, project) {
+    setTimeout(() => {
+      this.updateMetrics(nick, project);
+    }, 300000);
+  }
+
+}
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Dashboard for Archive Team Warrior
+![Screenshot of a dashboard with multiple line graphs for bandwidth](screenshots/screenshot.png)
+
+You got all excited about setting up Archive Team Warrior instances and helping out, but it's too much of a pain to make sure everything is running properly?
+
+I did, and instead of trying to hook into Grafana or any other more established solutions, I made my own.
+
+## Features
+
+* Reconnects to disconnected instances automatically.
+
+* Instances having issues are highlighted in red.
+
+* Pick one project to track and see your stats: how much data you've saved, and your position in the leaderboard.
+
+* An aggregate log that shows your instances are in fact doing work. 
+
+## Usage
+
+* When the page loads, it will ask you for information. Enter the NickName you are using for your instances, and the name of the project (used in the tracker url). It's case sensitive.
+
+* Enter your list of instance names and urls and press save. (Not: this list is not saved between sessions yet. If you have a large list of URLs, save it elsewhere to paste in).
+
+* Manage your instance by clicking it's name, where you will be taken to it's normal management page where you can adjust settings or shut down.
+
+* Enjoy your dashboard!
+
+## ToDo
+
+* Shutting down in-ui.
+
+* Saving settings between sessions
