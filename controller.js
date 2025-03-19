7c089cb7e38db6f12a2c2286782011d4bf0a4160
Abbie Gonzalez <abbiegonzalez@Enterprise.local>
2025-03-18 22:52:47 -0400

    saving settings, adding options to change nickname and add additional instances to dashboard after it starts running. added option to reset dashboard.

--- controller.js
+++ controller.js
@@ -3,26 +3,50 @@ class controller {
   #settingsDialog;
   warriorServerConnections;
   warriorServerDisconnects;
-  #topLevel;
   allSettings;
 
 
-  constructor() {
-    this.#topLevel = this;
-    this.allSettings = { nick : "", project: "", reuse: true};
+  constructor(savedSettings) {
+    document.App = this;
     this.#chartArea = document.getElementById('chartArea');
-    this.#settingsDialog = document.getElementById('settings');
+    this.#settingsDialog = document.getElementById('window-settings');
     this.warriorServerDisconnects = {};
     this.warriorServerConnections = new Array();
-    
-    this.showSettings();
-    document.getElementById('settingsSave').addEventListener("click", this.saveSettings.bind(this));
+
+    if(!savedSettings){
+      console.log(savedSettings);
+      this.allSettings = { nick : "", project: "", reuse: true, warriorServers: {}};
+      this.showSettings();
+    } else {
+      this.allSettings = savedSettings;
+      this.restoreSession(this.allSettings);
+    }
+
+    document.getElementById('window-settings-save').addEventListener("click", this.saveSettings.bind(this));
+    document.getElementById('toolbox-add-node').addEventListener("click", this.showWindow.bind(this));
+    document.getElementById('toolbox-change-nick').addEventListener("click", this.showWindow.bind(this));
+    document.getElementById('toolbox-reset').addEventListener("click", this.resetSettings.bind(this));
+
+    document.getElementById('window-node-add-addnode').addEventListener("click", this.windowAddNode_buttonAddNode_click.bind(this));
+    document.getElementById('window-change-nick-save').addEventListener("click", this.windowChangeNick_buttonSave_click.bind(this));
+    var closeButtons = document.getElementsByClassName('close-cancel');
+    Array.from(closeButtons).forEach(function(closeButton){
+      closeButton.addEventListener("click", document.App.window_closeButton_click);
+    });
     this.disconnectsInterval(this.warriorServerDisconnects);
   }
 
   addDisconnect(disconnectList, serverName, serverURL) {
     console.log(`${serverName} unavailable will check later.`);
     disconnectList[serverName] = serverURL;
+  }
+
+  showWindow(e) {
+    document.getElementById(e.target.dataset.windowOpen).classList.add("show");
+  }
+
+  hideWindow(windowID) {
+    document.getElementById(windowID).classList.remove("show");
   }
 
   showSettings(){
@@ -107,12 +131,61 @@ class controller {
             document.getElementById(item).remove();
           }
           this.warriorServerConnections.push(new anwarConnection( item, list[item], this.#chartArea, this, this.allSettings.reuse));
-
           delete list[item];
         }
       }
     }, 60000);
+  }
 
+  window_closeButton_click(e) {
+    document.App.window_close(document.getElementById(e.target.dataset.windowClose)); 
+  }
+
+  windowAddNode_buttonAddNode_click() {
+    var server = document.getElementById("serverConnectionString").value.split(",");
+    document.getElementById("serverConnectionString").value = "";
+    this.addSingleServer(server);
+    this.window_close(document.getElementById('window-add-node'));
+    localforage.setItem('dashSettings', this.allSettings).then(function(value) {
+      console.log(value);
+    });
+  }
+
+  windowChangeNick_buttonSave_click() {
+    var nickName = document.getElementById("window-change-nick-nickname").value;
+    this.allSettings.nick = nickName;
+    if (this.allSettings.project != ""){
+      this.updateMetrics(this.allSettings.nick, this.allSettings.project);
+    }
+    this.window_close(document.getElementById('window-change-nick'));
+  }
+
+  window_close(window) {
+    window.classList.add('close');
+    setTimeout(() => {
+      window.classList.remove('close');
+      window.classList.remove('show');
+    }, 500);
+  }
+
+  addSingleServer(server) {
+    this.warriorServerConnections.push(new anwarConnection( server[0].trim(), server[1], this.#chartArea, this));
+    this.allSettings.warriorServers[server[0]] = server[1];
+  }
+
+  restoreSession(settings) {
+    var warriorServers = settings.warriorServers;
+    if(Object.keys(warriorServers).length > 0) {
+      for(const item in warriorServers) {
+        this.warriorServerConnections.push(new anwarConnection( item, warriorServers[item], this.#chartArea, this));
+      }
+    }
+  }
+
+  resetSettings() {
+    localforage.clear().then(function() {
+      window.location.reload();
+    });
   }
 
   updateTimer(nick, project) {
